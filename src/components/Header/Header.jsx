637e264bf33ec55ec4a61484b1da48fcff1fb520
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-13 08:23:08 +0300

    basic Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,31 @@ import './Header.scss';
 class Header extends Component {
   render() {
     return (
-      <div className="App">
-      	header
+      <div>
+  	    <nav className="nav-extended">
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo">Logo</a>
+            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <ul className="side-nav" id="mobile-demo">
+              <li><a className="active" href="#test1">Home</a></li>
+              <li><a href="#test2">Skill</a></li>
+              <li><a href="#test2">Rortfolio</a></li>
+              <li><a href="#test2">Experience</a></li>
+              <li><a href="#test2">Education</a></li>
+              <li><a href="#test2">Contact</a></li>
+            </ul>
+          </div>
+          <div className="right nav-content hide-on-med-and-down">
+            <ul className="tabs tabs-transparent">
+              <li className="tab"><a className="active" href="#test1">Home</a></li>
+              <li className="tab"><a href="#test2">Skill</a></li>
+              <li className="tab"><a href="#test2">Rortfolio</a></li>
+              <li className="tab"><a href="#test2">Experience</a></li>
+              <li className="tab"><a href="#test2">Education</a></li>
+              <li className="tab"><a href="#test2">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
+
+
 var map;
 var marker;
 
