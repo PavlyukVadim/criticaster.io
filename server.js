ac7621fa492ddace4836e26a350b473c1857bf94
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-07-18 23:17:09 +0300

    fix routing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('./build'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
  	res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
