4d18be42479956178affe3317d62eafe77fee6aa
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-07-20 23:39:11 +0300

    add app config

---
+++ src/appConfig.js
@@ -0,0 +1,29 @@
+const config = {
+  navMenu: [{
+      title: 'Home',
+      link: '/',
+    },
+    {
+      title: 'Skills',
+      link: '/skills',
+    },
+    {
+      title: 'Portfolio',
+      link: '/portfolio',
+    },
+    {
+      title: 'Experience',
+      link: '/experience',
+    },
+    {
+      title: 'Education',
+      link: '/education',
+    },
+    {
+      title: 'Contact',
+      link: '/contact',
+    },
+  ],
+};
+
+export default config;
