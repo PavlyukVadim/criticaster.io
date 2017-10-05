1ca0cfdf747621cd5fdd81dd666a4dfcfb23e6d4
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-10-05 09:56:45 +0300

    update info

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './Experience.scss';
 
 let arrayOfExperience = [{
-  period: '2016 - Current',
-  company: 'SQDev',
-  position: 'Full-stack Developer',
-  desc: 'Now I\'m working as full-stack developer in sqDev, we create sites, e-shops, apps. Our technology stack consist of: HTML/SCSS, js/jQuery, React.js, Webpack, Gulp, Node.js, Express.js, WordPress.'
+  period: 'Jun 2017 - Current',
+  company: 'BSG',
+  position: 'Frontend Developer',
+  desc: 'Now I\'m working as a frontend developer in BSG. I work on insurance project. Our technology stack consist of: HTML/SCSS, js/jQuery, Webix.js, React.js, Webpack, Gulp.'
 },
 {
-  period: '2016 - 2017',
+  period: 'Oct 2016 - Jun 2017',
+  company: 'SQDev',
+  position: 'Full-stack Developer',
+  desc: 'I was working as a full-stack developer in sqDev, we were creating sites, e-shops, apps. Our technology stack consisted of: HTML/SCSS, js/jQuery, React.js, Webpack, Gulp, Node.js, Express.js, WordPress.'
+},
+{
+  period: 'Jan 2016 - Oct 2016',
   company: 'WeblancerNet',
   position: 'Web Developer',
   desc: 'I was working as a freelance web developer. I had projects with HTML/CSS (landing pages, sites), JS/jQuery (projects like constructor of products for e-shop)'
@@ -46,7 +52,7 @@ class Experience extends Component {
     return (
       <section id="experience">
         <div className="container">
-          <h2>1,5 Years Experience</h2>
+          <h2>2 Years Experience</h2>
           <div className="experience-timeline">
             {this.getExperience()}
           </div>
