cd22757ec8d3ada9a8fca40587a7b02e96aeaec5
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-01-28 16:48:37 +0200

    generate posts from markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-  ],
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'markdown-pages',
+      },
+    },
+    'gatsby-transformer-remark',
+  ]
 };
