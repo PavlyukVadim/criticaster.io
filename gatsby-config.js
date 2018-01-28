dbc90a56a1b215352f9e689b81dfdc788dc4876b
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-01-28 13:43:55 +0200

    remove path-prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  pathPrefix: "/pavlyuk.io",
   plugins: [
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-react-helmet',
