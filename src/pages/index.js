5e1171df2afb463848d9a429d9af3db386c651d4
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-01-28 17:13:41 +0200

    add list of markdown blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/counter/">Counter</Link>
-  </div>
-)
+const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <Link key={edge.node.id} to={edge.node.frontmatter.path}>AA</Link>);
 
-export default IndexPage
+  return <div>{Posts}</div>;
+};
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
