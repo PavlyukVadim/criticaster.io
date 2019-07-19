db64ad2d1d26ce803bd6dd1ee4db4d34a3b5eb34
PavlyukVadim <pavlyuk.dev@gmail.com>
2019-07-19 08:56:43 +0300

    migrate gatsby-link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby-link'
 import TemplateWrapper from '../components/layout'
 // import { DiscussionEmbed } from 'disqus-react'
 import config from './../config'
